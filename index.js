b38ac36038a128a310c04928cd3d2d990502498e
Christina Burchette <burchc426@gmail.com>
2019-01-19 20:35:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
-});
+  init()})
+  
+function init() {
+    let movieData =JSON.parse(movies);
+    console.log(movieData)
+     title.innerHTML = movieData.Titanic.title 
+     director.innerHTML = movieData.Titanic.director
+     genre.innerHTML = movieData.Titanic.genre
+     filmRating.innerHTML = movieData.Titanic.filmRating
+     poster.innerHTML = movieData.Titanic.poster
+     description.innerHTML = movieData.Titanic.description
+     audienceScore.innerHTML = movieData.Titanic.audienceScore
+}
+
+function terminate(){
+  let movieData =JSON.parse(movies)
+  title.innerHTML = movieData.title
+  
+};
+
+//will need to add event to get info to open on load
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
